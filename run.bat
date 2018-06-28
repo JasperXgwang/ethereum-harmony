c03ddaf733c711ef31668fd782277b6623b0f93d
JasperXgwang <jasperxgwang@126.com>
2018-06-28 17:41:40 +0800

    add private start bat

---
+++ run.bat
@@ -0,0 +1,3 @@
+java -Dethereumj.conf.res=private.conf -Ddatabase.name=database-private -DnetworkProfile=private -Dpeer.listen.port=30303 -Dserver.port=8080 -jar harmony.ether.camp.jar
+
+java -Dethereumj.conf.res=private.conf -Ddatabase.name=database-private_2 -DnetworkProfile=private -Dpeer.listen.port=30304 -Dserver.port=8088 -jar harmony.ether.camp.jar
